386d173afec22c36a8bc9d3d6bdd0140b8dda654
abhinav-pandey05 <35064909+abhinav-pandey05@users.noreply.github.com>
2018-01-03 20:36:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-// document.getElementById("RT").style.display = "none";
-
 document.getElementById("RT").style.display = "none";
 //IIFE
 var Age = (function () 
 {
     return   {
-        
-   
+        dispYear : function(){
+			var yob = Age.getAge();
+			document.getElementById("RT").innerHTML = "You were born in ---  " + yob;
+		},
         getAge : function() 
         {
-
+			document.getElementById("RT").style.display = "none";
             var num = document.getElementById("numb").value;
-            console.log('getAge INSIDE', num);
-            if (num == "" || num == 0) {
-                console.log("Please enter your age!!!");
+			if (num == "" || num == 0) {
+				window.alert("Please enter your age!!!");
+				return;
+            }else if (num <= 0 || num >= 110) {
+                window.alert("Enter a valid age ");
                 return;
-            }
-            else if (num <= 0 || num >= 110) {
-                console.log("Enter a valid age ");
-                return;
-            }
-            else{
-            var yob = 2017 - num;
-            document.getElementById("RT").style.display = "flex";
-            document.getElementById("RT").innerHTML = "You were born in ---" + yob;
-            // return yob;
-            }
+            }else{
+				var currentYear = new Date().getFullYear();
+				var yob = currentYear - num;
+				document.getElementById("RT").style.display = "flex";
+				
+                return yob;
+			}
         },
         init : function() {
             
-            document.getElementById("btn").onclick = this.getAge;
-        
+			document.getElementById("ageform").onsubmit = function(e){
+				Age.dispYear();
+				e.preventDefault();
+			}
         }
     }
     
